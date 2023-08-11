941fa54a3a0a74b2f5084b8847c2fb69c104a68d
Aravind Shajan <117029956+aravindshajan6@users.noreply.github.com>
2023-08-11 18:40:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,36 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-
-//   base:'/portfolio',
-
-//   plugins: [react()],
-//   server: {
-//     host: true,
-//     port: 8000
-//   }
-// })
-
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/react-portfolio',
-    server: {
-              host: true,
-              port: 8000
-            }
-  }
+export default defineConfig({
 
-  if (command !== 'serve') {
-    config.base = '/portfolio'
-  }
+  base:'/react-portfolio',
 
-  return config
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 8000
+  }
 })
+
+// vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ command }) => {
+//   const config = {
+//     plugins: [react()],
+//     base: '/react-portfolio',
+//     server: {
+//               host: true,
+//               port: 8000
+//             }
+//   }
+
+//   if (command !== 'serve') {
+//     config.base = '/portfolio'
+//   }
+
+//   return config
+// })
